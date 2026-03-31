aeb92e558a064834a5ecd6536314bcfec2ad6613
chhhhhhl <3994232910@qq.com>
2026-03-31 09:10:45 +0800

    feat：stargazing可显示但用户模块失效（WIP）

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,14 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  define: {
+    'process.env': {},
   },
   plugins: [
     tanstackRouter({
